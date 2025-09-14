7259cdfd9fa62e0a04ab701aca45d59db3ba1da5
Pythonic2 <89715071+Pythonic2@users.noreply.github.com>
2025-09-13 23:31:39 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = 'django-insecure-^0kbg8)m$7mcmjnlkjx@$)uo)$u4hyob2rxks3t21%c-u9qxtg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['donadochoppvendas-390560037832.southamerica-east1.run.app']
+ALLOWED_HOSTS = ['*']
 #teste
 CSRF_TRUSTED_ORIGINS = [
-    "https://donadochoppvendas-390560037832.southamerica-east1.run.app",
+    "*",
 ]
 #ALLOWED_HOSTS = ['*']
 # Application definition
