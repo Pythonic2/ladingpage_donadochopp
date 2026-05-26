13aa02484b0875e09e96168e680208969cd83175
Pythonic2 <igoormarinhosilva@gmail.com>
2026-05-26 13:40:09 -0300

    mudando porta

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ echo "Rodando makemigrations e migrate..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# Inicia o Gunicorn
-echo "Iniciando Gunicorn..."
 exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8010}
