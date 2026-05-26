85ff0c809d755976219e5d15e6c7a7a541a8810c
Pythonic2 <igoormarinhosilva@gmail.com>
2026-05-26 16:58:33 -0300

    teste build imagens

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 EXPOSE 8010
 # Comando para iniciar o servidor Django
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8010"]
+CMD ["sh", "-c", "python manage.py migrate && python manage.py runserver 0.0.0.0:8010"]
 #CMD exec python manage.py runserver 0.0.0.0:$PORT
 #RUN pip install gunicorn
 
--- core/settings.py
+++ core/settings.py
@@ -126,13 +126,10 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": os.environ.get("SQLITE_NAME", BASE_DIR / "db.sqlite3"),
         }
 
     }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
