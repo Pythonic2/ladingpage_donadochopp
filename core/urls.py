3224e0e94ac279976c44efabccb09e41c9e965bc
Pythonic2 <igoormarinhosilva@gmail.com>
2026-05-26 18:22:44 -0300

    rebuildd

--- core/urls.py
+++ core/urls.py
@@ -17,12 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('home/', admin.site.urls),
     path('', include('home.urls')),
 ]
 
-if settings.DEBUG or getattr(settings, "SERVE_MEDIA", False):
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, "SERVE_MEDIA", False):
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
