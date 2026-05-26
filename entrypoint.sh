167cac413b409effd72164909aa29c271223a385
Pythonic2 <igoormarinhosilva@gmail.com>
2026-05-26 14:27:14 -0300

    teste port

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Aplica migrações antes de iniciar o servidor
-echo "Rodando makemigrations e migrate..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8010}
